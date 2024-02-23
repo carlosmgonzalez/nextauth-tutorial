159cbc5fa3b4df77ac35aaf70058f8c885501036
Carlos Gonzalez <carlosmgonzalez1998@gmail.com>
2024-02-23 18:10:50 -0300

    listo para produccion, luego agrego una ultima funcionalidad

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         </h1>
         <p className="text-white text-lg">A siemple authentication service</p>
         <div>
-          <LoginButton asChild mode="modal">
+          <LoginButton asChild>
             <Button variant="secondary" size="lg">
               Sign in
             </Button>
--- lib/mail.ts
+++ lib/mail.ts
@@ -42,42 +42,54 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
-  const sender = process.env.AWS_SES_SENDER;
-
-  const params = {
-    Destination: {
-      CcAddresses: [email],
-      ToAddresses: [email],
-    },
-    Message: {
-      Body: {
-        Html: {
-          Charset: "UTF-8",
-          Data: `
-            <h1>
-              Verification Email
-            </h1>
-            <h2>
-              If you want to verify your email click 
-            </h2>
-            <a href="${confirmLink}">
-              HERE
-            </a>
-          `,
-        },
-      },
-      Subject: {
-        Charset: "UTF-8",
-        Data: "Confirm your email",
-      },
-    },
-    Source: `carlos@carlosgonzalez.info`,
-  };
-
   try {
-    await AWS_SES.sendEmail(params).promise();
-    console.log("email send");
+    await resend.emails.send({
+      from: "carlos@carlosgonzalez.info",
+      to: email,
+      subject: "Verify your email",
+      html: `<p>Click <a href="${confirmLink}">here</a> to verify your email</p>`,
+    });
   } catch (error) {
     console.log(error);
   }
 };
+
+// export const sendVerificationEmail = async (email: string, token: string) => {
+//   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+
+//   const params = {
+//     Destination: {
+//       CcAddresses: [email],
+//       ToAddresses: [email],
+//     },
+//     Message: {
+//       Body: {
+//         Html: {
+//           Charset: "UTF-8",
+//           Data: `
+//             <h1>
+//               Verification Email
+//             </h1>
+//             <h2>
+//               If you want to verify your email click
+//             </h2>
+//             <a href="${confirmLink}">
+//               HERE
+//             </a>
+//           `,
+//         },
+//       },
+//       Subject: {
+//         Charset: "UTF-8",
+//         Data: "Confirm your email",
+//       },
+//     },
+//     Source: `carlos@carlosgonzalez.info`,
+//   };
+
+//   try {
+//     await AWS_SES.sendEmail(params).promise();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
